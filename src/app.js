d308bd2d859f9ad8c4cd49e81d07aa8e68180931
jefersongsg <jefersonsilva5525@gmail.com>
2024-11-07 10:47:08 -0300

    criacao das pastas app,server

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+class App {
+    constructor(){
+        this.app = express()
+
+        this.middlewares();
+        this.routes();
+    }
+
+    middlewares(){
+        this.app.use(express.json())
+    }
+    routes() {}
+}
+
+module.exports = new App().app
