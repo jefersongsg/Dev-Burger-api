de268f6436ccb144bd4328530e070fae1dcc4819
jefersongsg <jefersonsilva5525@gmail.com>
2024-12-05 15:45:44 -0300

    tudo certo ate a session

---
+++ src/app.js
@@ -0,0 +1,26 @@
+
+import express from 'express';
+import routes from './routes';
+
+import './database';
+
+class App {
+    constructor() {
+        this.App = express()
+
+        this.middlewares();
+        this.routes();
+    }
+
+    middlewares() {
+        this.app.use(
+            express.json()
+        );
+    }
+    routes() {
+        this.app.use(routes);
+    }
+}
+
+export default new App().app
+
---
+++ src/server.js
@@ -0,0 +1,7 @@
+
+import app from './app'
+
+app.listen(3001, () => console.log('server is running at port 3001..:)'))
+
+
+
