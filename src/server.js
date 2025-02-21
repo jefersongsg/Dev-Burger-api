2995534096bf338fd4d2c0852f9b438a91e9e86a
jefersongsg <jefersonsilva5525@gmail.com>
2025-02-21 08:52:21 -0300

    test conection vercel

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ import app from './app.js';
 
 const port = process.env.PORT || 3001
 
-app.listen( () => console.log('server is running at port 3001..:)'));
+app.listen(3001, () => console.log('server is running at port 3001..:)'));
 
 
 
